f0197f36a320ac4716d9bee4231f64d89eb19258
sysashi <sysashi@hotmail.com>
2016-09-27 14:31:14 +0700

    Add active_path_link function for templates
    Plus some cleanup :)

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,6 +1,30 @@
 defmodule TinyFair.LayoutView do
   use TinyFair.Web, :view
 
+  def active_path_link(helper_fun, conn, opts, do: contents) do
+    current_route = existing_route?(conn_controller_action(conn), registered_routes())
+    to = helper_fun.(conn)
+    opts = if current_route.path == to do
+      Keyword.merge(opts, [class: "is-active"], fn :class, v1, v2 -> "#{v1} #{v2}" end)
+    else
+      opts
+    end |> Keyword.merge(to: to)
+    link(contents, opts)
+  end
+
+  defp existing_route?({controller, action}, routes) when is_list(routes) do
+    Enum.find(routes, fn route ->
+      route.plug == controller && route.opts == action end)
+  end
+
+  defp conn_controller_action(conn) do
+    action = conn.private.phoenix_action
+    controller = conn.private.phoenix_controller
+    {controller, action}
+  end
+
+  defp registered_routes, do: TinyFair.Router.__routes__
+
   def show_alerts(conn, key) when is_atom(key), do: show_alerts(conn, [key])
 
   def show_alerts(conn, keys) when is_list(keys) do
