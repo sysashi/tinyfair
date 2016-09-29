20682a24f30d78f6f9670793e8ef23b611f7c42e
sysashi <sysashi@hotmail.com>
2016-09-30 02:33:19 +0700

    Repo.count fun

--- lib/tinyfair/repo.ex
+++ lib/tinyfair/repo.ex
@@ -1,3 +1,11 @@
 defmodule TinyFair.Repo do
   use Ecto.Repo, otp_app: :tinyfair
+  import Ecto.Query
+
+  def count(%mod{} = struct, assoc) when is_atom(assoc) do
+    from(c in mod, where: c.id == ^struct.id,
+      join: o in assoc(c, ^assoc),
+      select: count(o.id),
+      group_by: c.id)
+  end
 end
